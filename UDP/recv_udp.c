4391d5ccf4aacb38c377dbe0e0ca0926a9b28c99
AuspiciousCrane <63011134@kmitl.ac.th>
2023-06-21 10:55:44 +0900

    Added: Initial Files and Makefile

---
+++ UDP/recv_udp.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+
+#define LISTEN_PORT 8889
+
+int main(void) {
+	int sock;
+	int i;
+	struct sockaddr_in addr;
+
+	int buf[8];
+
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(LISTEN_PORT);
+	addr.sin_addr.s_addr = INADDR_ANY;
+
+	bind(sock, (struct sockaddr*) &addr, sizeof(addr));
+
+	for (i = 0; i < 5; i++) {
+		// memset(buf, 0, sizeof(buf));
+		recv(sock, buf, sizeof(buf), 0);
+
+		printf("%d\n", buf[i]);
+	}
+
+	close(sock);
+
+	return 0;
+}
