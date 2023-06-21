8c63b25317b8fb7046e07d859319e129f64db9c4
AuspiciousCrane <63011134@kmitl.ac.th>
2023-06-21 11:43:59 +0900

    Added: Multithreading UDP recv basic

---
+++ UDP_multithread/recv_udp.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define LISTEN_PORT 8889
+
+void *listen_udp(void *args);
+
+int main(void) {
+	pthread_t thread_id;
+	pthread_create(&thread_id, NULL, listen_udp, NULL);
+	
+	for (int i = 0; i < 5; i++) {
+		sleep(1);
+		printf("Main Thread Working...\n");
+	}
+
+	pthread_join(thread_id, NULL);
+
+	printf("Exiting Program...\n");
+	return 0;
+}
+
+void *listen_udp(void *args) {
+	int sock;
+	int i;
+	struct sockaddr_in addr;
+
+	int buf[8];
+
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(LISTEN_PORT);
+	addr.sin_addr.s_addr = INADDR_ANY;
+
+	bind(sock, (struct sockaddr*) &addr, sizeof(addr));
+
+	for (i = 0; i < 5; i++) {
+		recv(sock, buf, sizeof(buf), 0);
+		printf("%d\n", buf[i]);
+	}
+
+	close(sock);
+}
---
+++ UDP_multithread/send_udp.c
@@ -0,0 +1,27 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define SERVER_ADDR "192.168.11.108"
+#define SERVER_PORT 8889
+
+int main(void) {
+	int sock, i;
+	struct sockaddr_in addr;
+	int buf[8];
+
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(SERVER_PORT);
+	addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);
+
+	for (i = 0; i < 5; i++) {
+		buf[i] = i;
+		sendto(sock, buf, sizeof(buf), 0, (struct sockaddr*) &addr, sizeof(addr));
+	}
+
+	close(sock);
+
+	return 0;
+}
