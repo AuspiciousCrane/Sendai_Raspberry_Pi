ba024a5d0477d7fe679d5faf6ee10c2845518c37
AuspiciousCrane <63011134@kmitl.ac.th>
2023-07-04 11:25:43 +0900

    Added: .h file

--- AnalogOutputAndCounterRead/AnalogOutputAndCounterRead.c
+++ AnalogOutputAndCounterRead/AnalogOutputAndCounterRead.c
@@ -3,21 +3,7 @@
 #include <unistd.h>
 #include <limits.h>
 
-#include "ccnt.h"
-#include "caio.h"
-
-#define COUNTER_NAME "CNT000"
-#define EXTERNAL_SIGNAL_SOURCE CNT_SIGTYPE_ISOLATE
-#define COUNT_DIRECTION CNT_DIR_UP
-#define FILTER_COEFFICIENT 0xA
-#define OPERATION_PHASE CNT_MODE_2PHASE
-#define OPERATION_MULTIPLIER CNT_MUL_X4
-#define OPERATION_CLEAR_SYNC CNT_CLR_ASYNC
-
-#define DAC_NAME "AIO001"
-#define DAC_CHANNEL 0
-#define DAC_VOLTAGE_OUTPUT 5
-#define DAC_OUTPUT_RANGE 50 // 50 is 0-10V
+#include "AnalogOutputAndCounterRead.h"
 
 short MaxChannels;
 short channel_no_array[10];
@@ -25,13 +11,6 @@ short counter_id;
 
 short dac_id;
 
-void initialize_counter(unsigned long count_data);
-void initialize_dac();
-
-void evaluate_return_code_cnt(long r_code);
-void evaluate_return_code_aio(long r_code);
-
-void print_count_value(unsigned long count);
 
 int main() {
 	long return_code;
